051e45c3feeb3e7ee09c0efffd0f2247b5ebba7b
MadheshLoganathan123 <loganathanmadhesh@gmail.com>
2026-08-09 13:30:08 +0530

    new commit

--- Desktop/DAV Lab/Experiment 3/Experiment 3B/exp3B_regression_analysis.py
+++ Desktop/DAV Lab/Experiment 3/Experiment 3B/exp3B_regression_analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, LogisticRegression
